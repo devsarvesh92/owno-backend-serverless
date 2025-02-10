77fa749679d0dce47552ac19aebddf6ca5949ea4
sarvesh sawant <sarvesh@ip-192-168-0-101.ec2.internal>
2025-02-10 17:20:32 +0530

    Owno serverless initial setup

---
+++ owno/app.py
@@ -0,0 +1,17 @@
+import json
+
+
+def lambda_handler(event, context):
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            {
+                "message": "hello world",
+                # "location": ip.text.replace("\n", "")
+            }
+        ),
+    }
+
+
+def register_user(event, context):
+    pass
